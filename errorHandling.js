0c695d42a8a2baf10d066d45c69e9af0a99bebdc
develectro <46164660+develectro@users.noreply.github.com>
2021-04-15 01:30:32 +0200

    Create errorHandling.js
    Example of Errors Handling in JavaScript

---
+++ errorHandling.js
@@ -0,0 +1,52 @@
+//Custom Error:
+
+var MyError = function(){
+    console.log("An error detected");
+    throw "this error is because you have entered a string";
+}
+
+
+function getName(name){
+  if(typeof(name) =="string"){
+      throw MyError();
+  }
+}
+
+getName('Mustafa');
+
+
+
+function Computer(myComputer){
+  if(typeof(myComputer) == "string"){
+      if(myComputer == 'DELL'){
+          console.log('It\'s DELL!');
+      }
+      if(myComputer == 'TOSHIBA'){
+        console.log('It\'s TOSHIBA!');
+    }
+     if(myComputer == 'hp'){
+        console.log('It\'s Helwet Packard!');
+    }else{  
+        console.log('Unrecognized!');
+    }
+  }else{
+      console.log("Cannot work because it's not a string");
+  }
+}
+Computer("DELL");
+
+ 
+function CapitalizeName(name){
+
+      try{
+          var x = name.toUpperCase();
+      }catch(error){
+        throw "This is a Syntax Error";
+      }finally{
+          console.log('Done')
+      }
+      return x;
+}
+
+console.log(CapitalizeName(890));
+
